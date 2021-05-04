3a694fb7f3cd90ad8d2569db0c32fc5b9e0b684f
qwx <qwx@sciops.net>
2021-05-04 03:30:42 +0000

    use opl2's actual sampling rate and don't buffer output
    - don't buffer output: rather than loop and use bio(2), just generate
    all at once and write immediately; this fixes realtime applications
    (from umbraticus' merged patch for games/opl3).
    - sanity checks and exit status
    - the opl2 chip has a weird sampling rate of 49.716kHz.  output rate
    is still audio(3)'s default 44.1kHz, and input rate is arbitrary
    (ultima6 m files: 60Hz, doom: same as opl chip).  instead of
    implementing downsampling ourselves, we let pcmconv take care of it.

--- opl2m.c
+++ opl2m.c
@@ -7,28 +7,31 @@ void	opl2wr(int, int);
 void	opl2init(int);
 
 enum{
-	Rate = 44100,
+	OPLrate = 49716,	/* 3579545Hz master clock / 72 */
 };
 
 void
 usage(void)
 {
-	fprint(2, "usage: %s [-n nsamp] [file]\n", argv0);
+	fprint(2, "usage: %s [-r rate] [file]\n", argv0);
 	exits("usage");
 }
 
 void
 main(int argc, char **argv)
 {
-	int r, v, dt, nsamp, fd;
-	uchar *sb, u[4];
-	Biobuf *bi, *bo;
+	int rate, n, r, v, fd, pfd[2];
+	uchar sb[65536 * 4], u[4];
+	double f, dt;
+	Biobuf *bi;
 
 	fd = 0;
-	nsamp = Rate / 700;
+	rate = OPLrate;
 	ARGBEGIN{
-	case 'n':
-		nsamp = Rate / atoi(EARGF(usage()));
+	case 'r':
+		rate = atoi(EARGF(usage()));
+		if(rate <= 0 || rate > OPLrate)
+			usage();
 		break;
 	default:
 		usage();
@@ -37,21 +40,41 @@ main(int argc, char **argv)
 		if((fd = open(*argv, OREAD)) < 0)
 			sysfatal("open: %r");
 	bi = Bfdopen(fd, OREAD);
-	bo = Bfdopen(1, OWRITE);
-	if(bi == nil || bo == nil)
+	if(bi == nil)
 		sysfatal("Bfdopen: %r");
-	nsamp *= 4;
-	if((sb = malloc(nsamp)) == nil)
-		sysfatal("malloc: %r");
-	opl2init(Rate);
-	while(Bread(bi, u, sizeof u) > 0){
+	opl2init(OPLrate);
+	if(pipe(pfd) < 0)
+		sysfatal("pipe: %r");
+	switch(rfork(RFPROC|RFFDG)){
+	case -1:
+		sysfatal("rfork: %r");
+	case 0:
+		close(0);
+		close(pfd[1]);
+		dup(pfd[0], 0);
+		execl("/bin/audio/pcmconv", "pcmconv", "-i", "s16c2r49716", "-o", "s16c2r44100", nil);
+		sysfatal("execl: %r");
+	default:
+		close(1);
+		close(pfd[0]);
+	}
+	f = (double)OPLrate / rate;
+	dt = 0;
+	while((n = Bread(bi, u, sizeof u)) > 0){
 		r = u[0];
 		v = u[1];
-		dt = u[3]<<8 | u[2];
 		opl2wr(r, v);
-		while(dt-- > 0){
-			opl2out(sb, nsamp);
-			Bwrite(bo, sb, nsamp);
+		dt += (u[3] << 8 | u[2]) * f;
+		while((n = dt) > 0){
+			if(n > sizeof sb / 4)
+				n = sizeof sb / 4;
+			dt -= n;
+			n *= 4;
+			opl2out(sb, n);
+			write(pfd[1], sb, n);
 		}
 	}
+	if(n < 0)
+		sysfatal("read: %r");
+	exits(nil);
 }
